d6044ed087c65b3447189017e1ea3cee35f7d581
Tanchik <negashovata@gmail.com>
2022-09-26 09:07:41 +0300

    добавлена 2 и 3 задачи

--- zadachi6.py
+++ zadachi6.py
@@ -4,6 +4,22 @@
 # - 7 -> да
 # - 1 -> нет
 
-day = int(input("Введите номер дня недели от 1 до 7: "))
-check_day = lambda day: "выходной" if day == 6 or day == 7 else "будний день"
-print(check_day(day))
+# day = int(input("Введите номер дня недели от 1 до 7: "))
+# check_day = lambda day: "выходной" if day == 6 or day == 7 else "будний день"
+# print(check_day(day))
+
+# 2.Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
+
+# # Пример:
+
+# - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
+
+# import math
+
+# factorial = int (input("Введите число: "))
+# print([math.factorial(i) for i in range(1,factorial +1)])
+
+# 3. Задайте список из n чисел последовательности (1 + 1/n)^n и выведите на экран их сумму.
+
+n = int(input('Введите число: '))
+print(round(sum([round((1 + 1 / x)**x, 3) for x in range(1, n + 1)]), 3))
